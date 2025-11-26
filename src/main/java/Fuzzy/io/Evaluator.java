cfc9712751d836c9ed15a09de45a8892560b4a16
hagarabdelmotalb <114605750+hagarabdelmotalb@users.noreply.github.com>
2025-11-27 00:54:48 +0200

    I/O evaluation pipeline

--- src/main/java/Fuzzy/io/Evaluator.java
+++ src/main/java/Fuzzy/io/Evaluator.java
@@ -1,22 +1,87 @@
 package Fuzzy.io;
 
+import Fuzzy.Defuzzy.DefuzzificationMethod;
 import Fuzzy.Rules.RuleBase;
 import Fuzzy.inference.InferenceEngine;
+import Fuzzy.operator.SNorm;
+import Fuzzy.operator.TNorm;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Evaluator {
     private InferenceEngine engine;
     private RuleBase ruleBase;
 
+    private DefuzzificationMethod defuzzifier;
+    private TNorm tnorm;
+    private SNorm snorm;
 
-    public Evaluator(InferenceEngine engine, RuleBase ruleBase) {
+
+    private Map<String, Map<String, Double>> fuzzifiedInputs;
+    private List<Map<String, Double>> ruleOutputs;
+    private Map<Double, Double> aggregatedOutput;
+
+
+    public Evaluator(InferenceEngine engine,
+                     RuleBase ruleBase,
+                     DefuzzificationMethod defuzzifier,
+                     TNorm tnorm,
+                     SNorm snorm) {
         this.engine = engine;
         this.ruleBase = ruleBase;
+        this.defuzzifier = defuzzifier;
+        this.tnorm = tnorm;
+        this.snorm = snorm;
     }
 
 
     public double evaluate(Map<String, Double> inputs) {
         return engine.evaluate(inputs, ruleBase);
+    }
+
+    private Map<Double, Double> aggregateOutputs(List<Map<String, Double>> ruleOutputs) {
+        Map<Double, Double> aggregated = new HashMap<>();
+
+        for (Map<String, Double> ruleOutput : ruleOutputs) {
+            for (Map.Entry<String, Double> entry : ruleOutput.entrySet()) {
+
+                double x = Double.parseDouble(entry.getKey());
+                double m = entry.getValue();
+
+                aggregated.putIfAbsent(x, 0.0);
+
+                double oldValue = aggregated.get(x);
+                double newValue = snorm.or(oldValue, m);
+
+                aggregated.put(x, newValue);
+            }
+        }
+        return aggregated;
+    }
+
+    public Map<String, Map<String, Double>> getFuzzifiedInputs() {
+        return fuzzifiedInputs;
+    }
+
+    public List<Map<String, Double>> getRuleOutputs() {
+        return ruleOutputs;
+    }
+
+    public Map<Double, Double> getAggregatedOutput() {
+        return aggregatedOutput;
+    }
+
+    public void setFuzzifiedInputs(Map<String, Map<String, Double>> data) {
+        this.fuzzifiedInputs = data;
+    }
+
+    public void setRuleOutputs(List<Map<String, Double>> data) {
+        this.ruleOutputs = data;
+    }
+
+    public void setAggregatedOutput(Map<Double, Double> data) {
+        this.aggregatedOutput = data;
     }
 }
